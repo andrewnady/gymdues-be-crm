f374ba75ab193a8e173a87aa35cef89d509660c2
Adeel Shehzad <adeellaradev@gmail.com>
2026-02-17 09:07:18 -0800

    Trending gyms API has been fixed

--- plugins/websquids/gymdirectory/controllers/api/GymsController.php
+++ plugins/websquids/gymdirectory/controllers/api/GymsController.php
@@ -149,7 +149,7 @@ class GymsController extends Controller {
       $fields = $request->input('fields');
       $sitemapOnly = ($fields === 'sitemap');
       $topGymsOnly = ($fields === 'topgyms');
-      $trending = $request->input('trending');
+      $trending = ($request->input('trending') == 'true') ? true : false;
 
       // Get address_id from query if specified (ignored when sitemapOnly)
       $addressId = $request->input('address_id');
